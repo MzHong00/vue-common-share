5a9eb576ac87c28d9d28af0c9bf4b2caaee3f990
JM <rkawk1539@gmail.com>
2025-06-21 22:13:05 +0900

    feat: 뒤로가기 버튼 재사용 컴포넌트 생성

---
+++ src/components/common/backButton/BackButton.vue
@@ -0,0 +1,24 @@
+<!-- 뒤로가기 버튼 -->
+<template>
+  <button @click="onClickGoBack">{{ label }}</button>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const { to, label = '뒤로가기' } = defineProps<{
+  to: string
+  label?: string
+}>()
+
+const router = useRouter()
+
+const onClickGoBack = () => {
+  // 🔁 히스토리가 없으면 홈으로 이동
+  if (!window.history.state.back) {
+    return router.replace(to)
+  }
+
+  router.back()
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <button @click="onClickAlertEnque">Alert Button</button>
+    <BackButton to="/back" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { useAlertAction } from '@/components/common/alert/alert.store'
+import BackButton from '@/components/common/backButton/BackButton.vue'
 
 const { alertEnqueue } = useAlertAction()
 
